1acfbc51136e3c2a8fb739e531dfc3a5fd752b00
fred.isaacs <fred.isaacs@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-04-13 06:23:56 +0000

    Added some configuration to blog module. Set ModulesController to read config from modules. Added default css from cakephp, just so we dont have to look at white/black for now.

--- trunk/library/Zupal/Module/Manager.php
+++ trunk/library/Zupal/Module/Manager.php
@@ -1,15 +1,38 @@
 <?php
+
 class Zupal_Module_Manager {
 	
-	public function getModuleNames() {
+	private $_moduleDir;
+	
+	public function __construct() 
+	{
+			$this->_moduleDir = 
+				dirname(Zend_Controller_Front::getInstance()->getModuleDirectory());	
+	}
+	
+	public function getModuleConfig($moduleName) 
+	{
+		$configFile = $this->_moduleDir . DS . $moduleName . DS . 'configuration.xml';
+		return new Zend_Config_Xml($configFile);				 		
+	}
+	
+	public function getModuleNames() 
+	{
 		$ignoreFiles = array(".svn","admin");
 		
 		$moduleNames = array();
-		$moduleDir = dirname(Zend_Controller_Front::getInstance()->getModuleDirectory());		
-		foreach (new DirectoryIterator($moduleDir) as $fileInfo) {	
+	
+		foreach (new DirectoryIterator($this->_moduleDir) as $fileInfo) 
+		{	
+			
 			if($fileInfo->isDot()) continue;  // ignore . and ..
-    		if(in_array($fileInfo->getFilename(),$ignoreFiles)) continue;  // ignore files
-    		$moduleNames[] = $fileInfo->getFilename();
+    		
+			if(in_array($fileInfo->getFilename(),$ignoreFiles)) continue;  // ignore files
+    		
+    		if($fileInfo->isDir()) 
+    		{
+    			$moduleNames[] = $fileInfo->getFilename();
+			}
 		}
 		return $moduleNames;
 	}
