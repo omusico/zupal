4b9e99c6cccee38b87ec344544f2063e0a8e093d
fred.isaacs <fred.isaacs@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-04-13 06:23:56 +0000

    Added some configuration to blog module. Set ModulesController to read config from modules. Added default css from cakephp, just so we dont have to look at white/black for now.

--- application/modules/admin/controllers/ModulesController.php
+++ application/modules/admin/controllers/ModulesController.php
@@ -3,11 +3,36 @@
 class Admin_ModulesController extends Zend_Controller_Action 
 {
 	
-	public function indexAction() {
-		
+	public function indexAction() 
+	{
+		$moduleConfigs = array();
+	
 		$manager = new Zupal_Module_Manager();
-		$moduleNames = $manager->getModuleNames();
-		Zend_Debug::dump($moduleNames, "ModuleNames:");
 		
+		foreach($manager->getModuleNames() as $moduleName) 
+		{
+			$config = $manager->getModuleConfig($moduleName);
+			$moduleConfigs[$moduleName] = $config->toArray();	
+		}	
+		
+		$this->view->moduleConfigs = $moduleConfigs;
 	}
+	
+	public function installAction() {}
+	
+	public function uninstallAction() 
+	{
+		$request = $this->getRequest();
+		if($request->getParam('moduleName',null) != null) {
+			$moduleName = $request->getParam('moduleName');
+			$message = sprintf("Module: %s uninstalled.", $moduleName);
+			Zend_Debug::dump($message, "Uninstall Message:");
+			exit();
+			$this->_helper->flashMessenger($message);
+		}
+	}
+	
+	public function enableAction() {}
+	
+	public function disableAction() {}
 }
