bec26218dc9df9f745cccee310b326eeae854539
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-04-25 20:50:05 +0000

    various refactoring to reflect Zend_DB; adding menu object.

--- application/models/Table/Zupal/Nodes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Table_Zupal_Nodes
-extends Zupal_Table_Abstract
-{
-	protected $_id_field = 'id'; // note this standard convention is NOT automatically overrriden by table definition.
-	protected $_name = 'nodes';
-	
-}
