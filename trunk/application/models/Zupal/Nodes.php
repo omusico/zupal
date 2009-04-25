61c910dede6a6cc431c89f785274baff85b53a4a
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-04-25 20:52:37 +0000

    various refactoring to reflect Zend_DB; adding menu object.

--- trunk/application/models/Zupal/Nodes.php
+++ trunk/application/models/Zupal/Nodes.php
@@ -3,4 +3,25 @@
 class Zupal_Nodes Extends Zupal_Domain_Abstract
 {
 
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ table_class @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+
+	/**
+	 * @see CPF_Formset_Domain::get_table_class()
+	 *
+	 */
+	protected function tableClass ()
+	{
+		return 'Zupal_Table_Nodes';
+	}
+
+	/**
+	 * @see CPF_Formset_Domain::get()
+	 *
+	 * @param unknown_type $pID
+	 * @return CPF_Formset_Domain
+	 */
+	public function get ($pID)
+	{
+		return new Zupal_Nodes($pID);
+	}
 }
---
+++ trunk/application/models/Zupal/Table/Nodes.php
@@ -0,0 +1,8 @@
+<?php
+
+class Table_Zupal_Nodes
+extends Zupal_Table_Abstract
+{
+	protected $_id_field = 'node_id';
+	protected $_name = 'nodes';	
+}
