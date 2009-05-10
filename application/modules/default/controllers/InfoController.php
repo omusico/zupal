615d18f620926319a5b8f44fa3f0b8f810badaff
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-05-10 07:56:25 +0000

    Completion of the logging module

---
+++ application/modules/default/controllers/InfoController.php
@@ -0,0 +1,29 @@
+<?
+
+class InfoController extends Zupal_Controller_Abstract
+{
+
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ showtimeAction @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+	/**
+	*
+	*/
+	public function showtimeAction ()
+	{
+	}
+
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ showdateAction @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+	/**
+	*
+	*/
+	public function showdateAction ()
+	{
+	}
+
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ versionAction @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+	/**
+	*
+	*/
+	public function showversionAction ()
+	{
+	}
+}
