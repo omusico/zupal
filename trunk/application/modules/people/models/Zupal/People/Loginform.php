6879b436857a2909295e854641c7b20efd6c3af9
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-05-12 16:53:43 +0000

    added panel branches; work on login form

--- trunk/application/modules/people/models/Zupal/People/Loginform.php
+++ trunk/application/modules/people/models/Zupal/People/Loginform.php
@@ -6,16 +6,11 @@ extends Zend_Form
 
 	public function __construct()
 	{
-
 		$ini_path = dirname(__FILE__) . DS . 'Loginform.ini';
 		$config = new Zend_Config_Ini($ini_path, 'fields');
-
 		parent::__construct($config);
-
-		$root = Zend_Controller_Front::getInstance()->getBaseUrl() . DS . 'people' . DS . 'user';
-
+		$root =  ZUPAL_BASEURL . DS . 'people' . DS . 'loginvalidate';
 		$this->setAction($root);
-
 		$this->setMethod('post');
 	}
 
