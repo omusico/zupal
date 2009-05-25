aa8f6fe41b20f15f84908d5d444b073dc6830902
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-05-25 04:18:25 +0000

    Table updates

---
+++ trunk/application/modules/admin/models/Zupal/Table/Modules.php
@@ -0,0 +1,33 @@
+<?php
+
+class Zupal_Table_Modules extends Zupal_Table_Abstract
+{
+
+    protected $_id_field = null;
+
+    protected $_name = 'zupal_modules';
+
+    public function create_table()
+    {
+        Zend_Db_Table_Abstract::getDefaultAdapter()->query("CREATE TABLE `zupal_modules` (
+          `name` varchar(100) NOT NULL,
+          `description` text NOT NULL,
+          `enabled` tinyint(4) NOT NULL default '0',
+          `required` tinyint(4) NOT NULL,
+          `version` varchar(50) NOT NULL,
+          `menu` varchar(100) NOT NULL,
+          `package` varchar(50) NOT NULL default '0',
+          `made` timestamp NOT NULL default CURRENT_TIMESTAMP,
+          PRIMARY KEY  (`name`)
+        ) ENGINE=MyISAM DEFAULT CHARSET=latin1");
+    }
+
+    protected function _init()
+    {
+        if(!$this->table_exists()) $this->create_table();
+    }
+
+
+}
+
+	
