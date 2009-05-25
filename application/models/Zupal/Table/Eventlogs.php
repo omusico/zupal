50db77e3d6259397fd0e6f187838a0c7af4f89d2
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-05-25 04:04:04 +0000

    various table updates - as well as realization of table creation code

--- application/models/Zupal/Table/Eventlogs.php
+++ application/models/Zupal/Table/Eventlogs.php
@@ -1,23 +1,33 @@
 <?php
 
-class Zupal_Table_Eventlogs
-extends Zupal_Table_Abstract
+class Zupal_Table_Eventlogs extends Zupal_Table_Abstract
 {
-	protected $_id_field = 'id';
-	protected $_name = 'zupal_eventlogs';
 
-	public static function install()
-	{
-		if ($this->table_exists()) return;
-		$this->getAdapter()->query('CREATE TABLE `zupal_eventlogs` (
-`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-`message` TEXT NOT NULL ,
-`priority` TINYINT NOT NULL ,
-`priorityName` VARCHAR( 20 ) NOT NULL ,
-`module` VARCHAR( 40 ) NOT NULL ,
-`action` VARCHAR( 40 ) NOT NULL ,
-`params` TEXT NOT NULL
-) ENGINE = MYISAM ;');
-	}
+    protected $_id_field = 'id';
+
+    protected $_name = 'zupal_eventlogs';
+
+    public function create_table()
+    {
+        Zend_Db_Table_Abstract::getDefaultAdapter()->query("CREATE TABLE `zupal_eventlogs` (
+          `id` int(10) unsigned NOT NULL auto_increment,
+          `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
+          `message` text NOT NULL,
+          `priority` tinyint(4) NOT NULL,
+          `priorityName` varchar(20) NOT NULL,
+          `module` varchar(40) NOT NULL,
+          `action` varchar(40) NOT NULL,
+          `params` text NOT NULL,
+          PRIMARY KEY  (`id`)
+        ) ENGINE=MyISAM AUTO_INCREMENT=231 DEFAULT CHARSET=latin1");
+    }
+
+    protected function _init()
+    {
+        if(!$this->table_exists()) $this->create_table();
+    }
+
+
 }
+
+	
--- application/modules/people/models/Zupal/Table/Employees.php
+++ application/modules/people/models/Zupal/Table/Employees.php
@@ -1,29 +1,34 @@
 <?php
 
-class Zupal_Table_Employees
-extends Zupal_Table_Abstract
+class Zupal_Table_Employees extends Zupal_Table_Abstract
 {
-	protected $_id_field = 'eid';
-	protected $_name = 'zupal_employees';
 
-	const INSTALL_TABLE = "CREATE TABLE `zupal_employees` (
-  `eid` int(11) NOT NULL auto_increment,
-  `node_id` int(11) NOT NULL,
-  `person_id` int(11) NOT NULL,
-  `manager` int(11) NOT NULL,
-  `position` varchar(100) NOT NULL,
-  `hire_date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `fire_date` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `salary` float NOT NULL,
-  `status` varchar(100) NOT NULL,
-  PRIMARY KEY  (`eid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
+    protected $_id_field = 'eid';
 
-	public static function install()
-	{
-		if (!$this->table_exists()):
-			Zupal_Database_Manager::get_adapter()->query(self::INSTALL_TABLE);
-		endif;
-	}
+    protected $_name = 'zupal_employees';
+
+    public function create_table()
+    {
+        Zend_Db_Table_Abstract::getDefaultAdapter()->query("CREATE TABLE `zupal_employees` (
+          `eid` int(11) NOT NULL auto_increment,
+          `node_id` int(11) NOT NULL,
+          `person_id` int(11) NOT NULL,
+          `manager` int(11) NOT NULL,
+          `position` varchar(100) NOT NULL,
+          `hire_date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+          `fire_date` timestamp NOT NULL default '0000-00-00 00:00:00',
+          `salary` float NOT NULL,
+          `status` varchar(100) NOT NULL,
+          PRIMARY KEY  (`eid`)
+        ) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1");
+    }
+
+    protected function _init()
+    {
+        if(!$this->table_exists()) $this->create_table();
+    }
+
 
 }
+
+	
