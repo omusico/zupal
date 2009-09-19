7cc7b060c07bfc2f30d23d4d1160eb23cb8242bc
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-09-19 18:56:08 +0000

    cleanup

--- application/modules/administer/controllers/ModulesController.php
+++ application/modules/administer/controllers/ModulesController.php
@@ -1,95 +1,38 @@
 <?php
-class Administer_ModulesController extends Zupal_Controller_Abstract
-{
-    /**
-     * @return string
-     * 
-     * 
-     */
-    public function init()
-    {
+class Administer_ModulesController extends Zupal_Controller_Abstract {
+/**
+ * @return string
+ *
+ *
+ */
+    public function init() {
         $this->_helper->layout->setLayout('admin');
-        
-                
-        
-                        parent::init();
+        parent::init();
     }
     /**
-     * 
+     *
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         $m = Administer_Model_Modules::getInstance();
-        
-                
-        
-                        $m->update_from_filesystem();
-        
-                
-        
-                        $this->view->modules = $m->findAll('folder');
-    }
-    public function activateAction()
-    {
-        $module_name = $this->_getParam('module_name');
-        
-                
-        
-                
-        
-                
-        
-                        $module = Administer_Model_Modules::getInstance()->get($module_name);
-        
-                
-        
-                        if (!$module):
-        
-                
-        
-                            $this->_forward('index', 'error', 'default', array(sprintf(__METHOD__ . ': no module named "%s"', $module_name)));
-        
-                
-        
-                        endif;
-        
-                
-        
-                
-        
-                
-        
-                        $on = $this->_getParam('on');
-        
-                
-        
-                        
-        
-                
-        
-                        $module->active = $on ? 1 : 0;
-        
-                
-        
-                
-        
-                
-        
-                        $module->save();
-        
-                
-        
-                
-        
-                
-        
-                        $this->_forward('index', NULL, NULL, array('message' => sprintf('active status of module "%s" set to "%s"', $module_name, $on ? 'YES' : 'NO')));
-    }
-    public function menueditAction()
-    {
-    }
-    public function menustoreAction()
-    {
+        $m->update_from_filesystem();
+        $this->view->modules = $m->findAll('folder');
     }
 
+    public function activateAction() {
+        $module_name = $this->_getParam('module_name');
+        $module = Administer_Model_Modules::getInstance()->get($module_name);
+        if (!$module):
+            $this->_forward('index', 'error', 'default', array(sprintf(__METHOD__ . ': no module named "%s"', $module_name)));
+        endif;
+        $on = $this->_getParam('on');
+        $module->active = $on ? 1 : 0;
+        $module->save();
+        $this->_forward('index', NULL, NULL, array('message' => sprintf('active status of module "%s" set to "%s"', $module_name, $on ? 'YES' : 'NO')));
+    }
+
+    public function menueditAction() {
+    }
+
+    public function menustoreAction() {
+    }
 }
