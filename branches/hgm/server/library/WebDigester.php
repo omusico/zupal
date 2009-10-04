75fb442aa2a3c83d15fb07aa5a7b650d3ceb2419
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-10-04 12:14:17 +0000

    renamed server

---
+++ branches/hgm/server/library/WebDigester.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of WebDigester
+ *
+ * @author bingomanatee
+ */
+class WebDigester {
+    //put your code here
+}
