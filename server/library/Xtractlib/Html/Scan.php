d0f9b9abc141074fe68573f5b23cc0042d727f58
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-10-04 12:06:18 +0000

    renamed server

---
+++ server/library/Xtractlib/Html/Scan.php
@@ -0,0 +1,39 @@
+<?
+
+class Xtractlib_Html_Scan
+{
+    public static function scan(Xtract_UrlHtml $html)
+    {
+        $html->save();
+        $dom = new Zend_Dom_Query($html->html);
+
+        $links = $dom->query('a');
+
+        foreach(Xtract_Model_UrlLinks::getInstance()->find(array('from_url'  => $html->in_url)) as $link):
+            $link->delete();
+        endforeach;
+
+        error_log("\n\n\n ************** \n\n\n" . __METHOD__ . ': url = ' . $html->in_url);
+        foreach($links as $link):
+            if (!($href =  $link->getAttribute('href'))):
+                continue;
+            endif;
+
+            error_log('LINK: ' . $href);
+            $url = $html->url();
+            Xtractlib_Html_Link::link_to($url, $href, $html);
+        endforeach;
+        $images = $dom->query('img');
+
+        foreach($images as $img):
+            error_log(__METHOD__ . ': image');
+
+            if (!($src = $img->getAttribute('src'))):
+                continue;
+            endif;
+
+            error_log('IMAGE: ' . $src);
+            Xtract_Model_UrlImages::make($html->url(), $src, $html);
+        endforeach;
+    }
+}
