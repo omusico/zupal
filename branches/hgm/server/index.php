fb46927b918cc816f3836042d69daa7b5e401904
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-10-04 12:11:03 +0000

    renamed server

--- branches/hgm/server/index.php
+++ branches/hgm/server/index.php
@@ -1,25 +1,6 @@
-<?php
-define ('ZF_LIB', '/users/bingomanatee/documents/sites/ZendFramework/library');
+<?
 
-set_include_path(ZF_LIB . PATH_SEPARATOR
-    . dirname(__FILE__) . '/library' . PATH_SEPARATOR
-    . get_include_path());
+include 'init.php';
 
-error_log(__FILE__ . ': includes = '
-    . str_replace(PATH_SEPARATOR, "\n: ", get_include_path()));
+include 'api.php';
 
-define('APPLICATION_PATH', dirname(__FILE__));
-define ('APPLICATION_ENV', 'production');
-
-/** Zend_Application */
-require_once 'Zend/Application.php';
-
-$application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/app.ini'
-);
-
-global $_zend_loaded;
-$_zend_loaded = TRUE;
-
-$application->setBootstrap(dirname(__FILE__) . '/Bootstrap.php')->bootstrap();
