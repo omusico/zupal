bc51ee8fac145484c0be69e80f5a1137c809fdfb
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-10-13 13:40:36 +0000

    Adding  CKeditor

---
+++ branches/v2/public/ckeditor/config.js
@@ -0,0 +1,29 @@
+﻿/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+config.toolbar = 'Medium';
+
+config.toolbar_Medium =
+[
+    ['Source',/*'-','Save','NewPage',/*'Preview','-','Templates'*],
+    ['Cut','Copy','Paste','PasteText','PasteFromWord'/*,'-','Print', ,'SpellChecker', 'Scayt'*/],
+    [/*'Undo','Redo','-',*/'Find','Replace','SelectAll','RemoveFormat'],
+   // ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+    '/',
+    ['Bold','Italic','Underline',/*'Strike',*/'Subscript','Superscript'],
+    ['NumberedList','BulletedList','-','Outdent','Indent'/*,'Blockquote'],
+    [*/,'JustifyLeft','JustifyCenter','JustifyRight'/*,'JustifyBlock'*/],
+    ['Link','Unlink','Anchor'],
+    [/*'Styles',*/'Format','Font','FontSize'],
+     '/',
+     ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar'/*,'PageBreak'*/],
+
+    ['TextColor','BGColor'/*],
+    [*/,'Maximize', 'ShowBlocks'/*,'-','About'*/]
+];
+
+};
