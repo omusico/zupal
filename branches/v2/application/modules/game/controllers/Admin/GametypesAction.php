05de32f4275b71600cff98defda4d7810b49b517
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-11-29 22:59:44 +0000

    Added generic games module

---
+++ branches/v2/application/modules/game/controllers/Admin/GametypesAction.php
@@ -0,0 +1,42 @@
+<?
+
+class Games_Admin_Gametypeaction
+extends Zupal_Action_CrudAbstract {
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ list @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+
+    public function store() {
+        $params = array('active' => 1);
+        $items = $this->_model()->find($params);
+
+        $data = array();
+
+        foreach($items as $item):
+            $row = $item->toArray();
+            $row['title'] = $item->get_title();
+            $data[] = $row;
+        endforeach;
+
+        $this->get_controller()->_store('id', $data, 'title');
+    }
+
+    public function newresponse () {
+
+    }
+
+    public function editresponse() {
+
+    }
+
+    public function deleteitem() {
+
+    }
+
+    public function deleteresponse() {
+
+    }
+
+    protected function _model_class() { return 'Game_Model_Gametypes'; }
+    
+    protected function _form_class() { return 'Game_Form_Gametypes'; }
+
+}
---
+++ branches/v2/application/modules/game/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?
+
+class Game_IndexController
+extends Zupal_Controller_Abstract
+{
+
+
+/* @@@@@@@@@@@@@ EXTENSION BOILERPLATE @@@@@@@@@@@@@@ */
+
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ controller_dir @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+    /**
+     *
+     * @return string
+     */
+    public function controller_dir () {
+        return dirname(__FILE__) . DIRECTORY_SEPARATOR;
+    }
+
+}
