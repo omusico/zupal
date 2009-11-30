3b662434001f99ad73738c2478caf29e781317d2
poppin.fressh <poppin.fressh@d6c98f56-2781-11de-a2f3-1373824ab735>
2009-11-30 01:08:29 +0000

    Work on Game admin

--- branches/v2/application/modules/game/forms/Gametypes.php
+++ branches/v2/application/modules/game/forms/Gametypes.php
@@ -3,7 +3,7 @@
 class Game_Form_Gametypes extends Zupal_Fastform_Domainform {
 
     protected function _domain_class() {
-        return 'Games_Model_Gametypes';
+        return 'Game_Model_Gametypes';
     }
 
     protected function _ini_path() {
